11ec8826c15116cf09079d2f2ff08e0e475d75f0
blitz <dj.debnath2000@gmail.com>
2021-09-14 20:59:05 +0530

    updated s3 module

--- S3/s3.py
+++ S3/s3.py
@@ -1,39 +1,37 @@
+from types import prepare_class
 import boto3
 import random
-import string
-from botocore.exceptions import ClientError
 
-# bucket_name="boto3-"+''.join(random.choices(string.ascii_lowercase+string.digits,k=10))
-
-#create a bucket
-# s3.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={
-#     'LocationConstraint':'ap-south-1'
-# })
-
-def createbucket(region=None):
+def createbucket():
     s3=boto3.client('s3')
-    bucket_name="boto3-"+''.join(random.choices(string.ascii_lowercase+string.digits,k=10))    
-    try:
-        if region is None:
-            s3.create_bucket(Bucket=bucket_name)
-            response=s3.get_bucket_location(Bucket=bucket_name)
-            print("Bucket created successfully - {} in region {}".format(bucket_name,response['LocationConstraint']))
-        else:
-            s3.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={
-                'LocationConstraint':region
-                })
-            response=s3.get_bucket_location(Bucket=bucket_name)
-            print("Bucket created successfully - {} in region {}".format(bucket_name,response['LocationConstraint']))
+    bname=input("Enter bucket name:")
+    bname=bname+"-"+str(random.randint(10000,100000))
+    s3.create_bucket(Bucket=bname)
 
-    except ClientError as e:
-        print(e)
-createbucket(region="ap-south-1")
+def listbucket():
+    s3=boto3.client('s3')
+    response=s3.list_buckets()
+    buckets = [bucket['Name'] for bucket in response['Buckets']]
+    for i in range(len(buckets)):
+        print("{}] {}".format(i+1,buckets[i]))
 
-# #print all bucket names
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
+def deletebucket():
+    pass
 
-#upload new file
-# data=open('a.txt','rb')
-# s3.Bucket('kops-504').put_object(Key='a.txt',Body=data)
+a=0
+while(a!=4):
+    print()
+    print("S3 control panel:")
+    print("1)Create bucket")
+    print("2)Delete bucket")
+    print("3)List bucket")
+    print("4)Quit")
+    a=int(input("Input option:"))
+    print()
+    if a==1:
+        createbucket()
+    elif a==2:
+        deletebucket()
+    elif a==3:
+        listbucket()
 
