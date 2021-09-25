f3b8ed5ecb3856bf3e3809a756a5b554914eb1d3
blitz <dj.debnath2000@gmail.com>
2021-09-25 19:26:53 +0530

    delete objects in s3 bucket

--- S3/s3.py
+++ S3/s3.py
@@ -1,4 +1,3 @@
-from types import prepare_class
 import boto3
 import random
 
@@ -16,17 +15,50 @@ def listbucket():
         print("{}] {}".format(i+1,buckets[i]))
 
 def deletebucket():
-    pass
+    listbucket()
+    print()
+    bucketname=input("Enter bucket name:")
+    b=0
+    while(b!=5):
+        print("1)List all objects in bucket")
+        print("2)Empty the bucket")
+        print("3)Delete particular object in bucket")
+        print("4)Delete bucket")
+        print("5)Quit")
+        b=int(input("Choose option:"))
+        s3=boto3.client('s3')
+        if b==1:            
+            list=s3.list_objects(Bucket=bucketname)["Contents"]
+            print()
+            print("Bucket contents are:")
+            for obj in list:
+                print(obj["Key"])
+        elif b==2:
+            list=s3.list_objects(Bucket=bucketname)["Contents"]
+            print()
+            print("Deleting all objects in {}".format(bucketname))
+            for obj in list:
+                print("Deleting - {}".format(obj["Key"]))
+                s3.delete_object(Bucket=bucketname,Key=obj["Key"])
+        elif b==3:
+            list=s3.list_objects(Bucket=bucketname)["Contents"]
+            print()
+            print("Deleting only one object in {}".format(bucketname))
+            for obj in list:
+                print(obj["Key"])
+            s3.delete_object(Bucket=bucketname,Key=input("Enter object name to be deleted:"))
+
 
 a=0
-while(a!=4):
+while(a!=5):
     print()
     print("S3 control panel:")
     print("1)Create bucket")
     print("2)Delete bucket")
     print("3)List bucket")
-    print("4)Quit")
-    a=int(input("Input option:"))
+    print("4)Upload files in bucket")
+    print("5)Quit")
+    a=int(input("Choose option:"))
     print()
     if a==1:
         createbucket()
@@ -34,4 +66,5 @@ while(a!=4):
         deletebucket()
     elif a==3:
         listbucket()
-
+    elif a==4:
+        pass
