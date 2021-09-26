21eb78c66a33eba5bbdae304e760955caea98792
blitz <dj.debnath2000@gmail.com>
2021-09-26 16:47:53 +0530

    s3 code updated

--- S3/s3.py
+++ S3/s3.py
@@ -18,6 +18,7 @@ def deletebucket():
     listbucket()
     print()
     bucketname=input("Enter bucket name:")
+    print()
     b=0
     while(b!=6):
         print("1)List all objects in bucket")
@@ -27,35 +28,38 @@ def deletebucket():
         print("5)Delete bucket")
         print("6)Quit")
         b=int(input("Choose option:"))
+        print()
         s3=boto3.client('s3')
         if b==1:            
-            list=s3.list_objects(Bucket=bucketname)["Contents"]
-            print()
+            list=s3.list_objects(Bucket=bucketname)["Contents"]            
             print("Bucket contents are:")
             for obj in list:
                 print(obj["Key"])
+            print()
         elif b==2:
             list=s3.list_objects(Bucket=bucketname)["Contents"]
-            print()
             print("Deleting all objects in {}".format(bucketname))
             for obj in list:
                 print("Deleting - {}".format(obj["Key"]))
                 s3.delete_object(Bucket=bucketname,Key=obj["Key"])
+            print()
         elif b==3:
             list=s3.list_objects(Bucket=bucketname)["Contents"]
-            print()
             print("Deleting only one object in {}".format(bucketname))
             for obj in list:
                 print(obj["Key"])
             s3.delete_object(Bucket=bucketname,Key=input("Enter object name to be deleted:"))
+            print("Object deleted successfully")
+            print()
         elif b==4:
             s3.delete_bucket_policy(Bucket=bucketname)
-            print()
             print("{} bucket policy deleted successfully".format(bucketname))
+            print()
         elif b==5:
             s3.delete_bucket(Bucket=bucketname)
-            print()
             print("{} bucket deleted successfully".format(bucketname))
+            print()
+            break
 
 def uploadobject():
     s3=boto3.client('s3')
@@ -63,7 +67,7 @@ def uploadobject():
     print()
     bucketname=input("Enter bucket name:")
     print("Filename should include extension also and should be present in current working directory")
-    filename=input("Filename to be uploaded")
+    filename=input("Name of the file to be uploaded:")
     s3.upload_file(Filename=filename,Bucket=bucketname,Key=filename)
 
 a=0
@@ -71,17 +75,18 @@ while(a!=5):
     print()
     print("S3 control panel:")
     print("1)Create bucket")
-    print("2)Delete bucket")
-    print("3)List bucket")
-    print("4)Upload files in bucket")
+    print("2)List bucket")
+    print("3)Upload files in bucket")
+    print("4)Delete bucket")
     print("5)Quit")
     a=int(input("Choose option:"))
     print()
     if a==1:
         createbucket()
     elif a==2:
-        deletebucket()
-    elif a==3:
         listbucket()
-    elif a==4:
+    elif a==3:
         uploadobject()
+    elif a==4:
+        deletebucket()
+        
